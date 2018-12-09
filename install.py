4802afb77a8dc1bdd8d03f538d376a263ff5fc31
Vishal Maurya <vishalmr96@gmail.com>
2018-12-10 01:30:56 +0530

    Completely remove os.system deprecated call

--- install.py
+++ install.py
@@ -11,20 +11,20 @@ os.call(["cp", "-y", ".vimrc", home])
 # find the distro and install vim in it
 if find_executable("vim") is None :
     print ("Installing Vim")
-    distroName =  os.popen("lsb_release -a").read()
+    distroName =  process.popen("lsb_release -a").read()
     if "Fedora" in distroName :
-        os.system("sudo dnf install vim")
+        os.call(["sudo", "dnf", "install", "vim"])
     elif "Ubuntu" in distroName :
-        os.system("sudo apt install vim")
+        os.call(["sudo" "apt" "install" "vim"])
     elif "Arch" in distroName :
-        os.system("sudo pacman -S vim")
+        os.call(["sudo", "pacman", "-S", "vim"])
     elif "Antergos" in distroName :
-        os.system("sudo pacman -S vim")
+        os.call(["sudo", "pacman", "-S", "vim"])
 
 home = home + "/.vim"
 
 # create .vim directory, if not available
-if not os.path.isdir(home) :
+if not process.path.isdir(home) :
     os.call(["mkdir", home])
 
 # now copy all the files in .vim directory, to set up the working environment
