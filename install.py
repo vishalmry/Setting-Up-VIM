bc0238a2724f65a9e0964b8251f0d33c91c9272c
Vishal Maurya <vishalmr96@gmail.com>
2018-12-10 00:12:45 +0530

    Add install script for Vim and setting up its configuration

---
+++ install.py
@@ -0,0 +1,30 @@
+# install script
+import os
+from distutils.spawn import find_executable
+
+home = os.path.expanduser("~")
+
+# setting up the vimrc
+os.system("cp -i .vimrc " + home)
+
+# find the distro and install vim in it
+if find_executable("vim") is None :
+    print ("Installing Vim")
+    distroName =  os.popen("lsb_release -a").read()
+    if "Fedora" in distroName :
+        os.system("sudo dnf install vim")
+    elif "Ubuntu" in distroName :
+        os.system("sudo apt install vim")
+    elif "Arch" in distroName :
+        os.system("sudo pacman -S vim")
+    elif "Antergos" in distroName :
+        os.system("sudo pacman -S vim")
+
+home = home + "/.vim"
+
+# create .vim directory, if not available
+if not os.path.isdir(home) :
+    os.system("mkdir " + home)
+
+# now copy all the files in .vim directory, to set up the working environment
+os.system("cp -r * " + home)
