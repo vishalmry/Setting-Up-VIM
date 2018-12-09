360a315d734486b4c8ae003a861847c7f69b7aac
Vishal Maurya <vishalmr96@gmail.com>
2018-12-10 02:01:43 +0530

    Created a proper installation script, printed outputs

--- install.py
+++ install.py
@@ -6,7 +6,9 @@ from distutils.spawn import find_executable
 home = process.path.expanduser("~")
 
 # setting up the vimrc
-os.call(["mv", ".vimrc", home])
+print ("Copying VIMRC!!")
+os.call(["cp", ".vimrc", home])
+print ("Copying VIMRC finished!!")
 
 # find the distro and install vim in it
 if find_executable("vim") is None :
@@ -20,15 +22,20 @@ if find_executable("vim") is None :
         os.call(["sudo", "pacman", "-S", "vim"])
     elif "Antergos" in distroName :
         os.call(["sudo", "pacman", "-S", "vim"])
+    print ("Finished Installing!!")
 
 home = home + "/.vim"
 
 # create .vim directory, if not available
 if not process.path.isdir(home) :
+    print ("Creating .vim")
     os.call(["mkdir", home])
 
 # now copy all the files in .vim directory, to set up the working environment
+print ("Copying the plugins and configurations!!")
 os.call("cp -r * ~/.vim", shell = True)
 
 # install all the required plugins
+print ("Vim Plug Is Installing the Plugins, Please Wait....")
 os.call('vim -c ":PlugInstall" -c ":qa"', shell = True, stdout = os.PIPE, stderr = os.PIPE)
+print ("Plugins Installed.... Enjoy VIM!!")
